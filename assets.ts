52fbbe15524c1088f8ef53fb50ed567813a3fa46
koro <ryusama6996@gmail.com>
2023-06-08 22:25:06 +0530

    fix(src): remove logs

--- assets.ts
+++ assets.ts
@@ -3,9 +3,6 @@ export function getAssetPATH(){
     let env = dotenv.config({path: __dirname+'/.env'});
     let assets = env.parsed;
     let project = process.env["PROJECT"] || process.env["DEFAULT_PROJECT"];
-    console.log(project);
-    console.log(assets);
     let projectAsset = `${project}_ASSETS`;
-    console.log(projectAsset);
     return assets[projectAsset];
 }
